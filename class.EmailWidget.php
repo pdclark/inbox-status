f79a292a5492d2053ae31fb3e2c7c7d89e0bd4b5
Ben Lobaugh <ben@lobaugh.net>
2013-10-24 03:41:44 +0000

    Adding email widget

---
+++ class.EmailWidget.php
@@ -0,0 +1,88 @@
+<?php
+
+class BlobEmailWidget extends WP_Widget {
+
+	public function __construct() {
+	}
+
+        public function widget( $args, $instance ) {
+	    $defaults = array(
+		'server' => '',
+		'user' => '',
+		'title' => '',
+		'pass' => '',
+	    );
+	    $instance = wp_parse_args( $instance, $defaults );
+
+	    $e = BlobImap();
+	    $e->create_connection( $instance['server'], $instance['user'], $instance['pass'] );
+
+
+	    echo $args['before_widget'];
+	    
+	    echo $args['before_title'];
+	    echo $instance['title'];
+	    echo $args['after_title'];
+
+	    echo "<p>";
+	    echo $e->count_unread() . '/' . $e->count_all();
+	    echo "<br/>Unread/All";
+	    echo "</p>":
+
+	    echo $args['after_widget'];
+	    
+	}
+
+	public function form( $instance ) {
+	    $defaults = array(
+		'server' => '',
+		'user' => '',
+		'title' => '',
+		'pass' => '',
+	    );
+
+	    $instance = wp_parse_args( $instance, $defaults );
+
+	    ?>
+	    <p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" 
+		    id="<?php echo $this->get_field_id( 'title' ); ?>" 
+		    name="<?php echo $this->get_field_name( 'title' ); ?>"
+		    type="text"
+		    value="<?php echo esc_attr( $instance['title'] ); ?>"
+		/>
+	    </p>
+	    <p>
+		<label for="<?php echo $this->get_field_id( 'server' ); ?>"><?php _e( 'Server:' );?></label>
+		<input class="widefat"
+			name="<?php echo $this->get_field_name( 'server' ); ?>"
+			type="text"
+			value="<?php echo esc_attr( $instance['server'] ); ?>"
+		/>
+	    </p>
+	    <p>
+		<label for="<?php echo $this->get_field_id( 'user' ); ?>"><?php _e( 'User:' );?></label>
+		<input class="widefat"
+			name="<?php echo $this->get_field_name( 'user' ); ?>"
+			type="text"
+			value="<?php echo esc_attr( $instance['user'] ); ?>"
+		/>
+	    </p>
+	    <p>
+		<label for="<?php echo $this->get_field_id( 'pass' ); ?>"><?php _e( 'Pass:' );?></label>
+		<input class="widefat"
+			name="<?php echo $this->get_field_name( 'pass' ); ?>"
+			type="text"
+			value="<?php echo esc_attr( $instance['pass'] ); ?>"
+		/>
+	    </p>
+	    <?php 
+	}
+
+									    }
+} // end class
+
+add_action( 'widgets_init', function(){
+         register_widget( 'BlobEmailWidget' );
+});
--- email-counts.php
+++ email-counts.php
@@ -2,10 +2,11 @@
 /*
     Plugin Name: Email Counts
     Plugin URI: https://github.com/blobaugh/email-counts
-    Description: Easy shortcode and functions to show email counts in your theme (Like unread email)
+    Description: Easy widget and functions to show email counts in your theme (Like unread email)
     Version: 0.1
     Author: Ben Lobaugh
     Author URI: http://ben.lobaugh.net
 */
 
-
+require_once( 'class.BlobImap.php' );
+require_once( 'class.EmailWidget.php ' );

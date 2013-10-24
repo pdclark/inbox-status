37e3869aa170551c81b0af63ede58c1e1b2e5e68
Ben Lobaugh <ben@lobaugh.net>
2013-10-24 03:05:10 +0000

    Adding plugin file

---
+++ email-counts.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Plugin Name: Email Counts
+    Plugin URI: https://github.com/blobaugh/email-counts
+    Description: Easy shortcode and functions to show email counts in your theme (Like unread email)
+    Version: 0.1
+    Author: Ben Lobaugh
+    Author URI: http://ben.lobaugh.net
+*/
+
+

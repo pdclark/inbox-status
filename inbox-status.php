5b5337877da1be2e983af00a21b2425901ea0131
Ben Lobaugh <ben@lobaugh.net>
2013-10-24 04:05:01 +0000

    Adding new plugin file

---
+++ inbox-status.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: Inbox Status 
+    Plugin URI: https://github.com/blobaugh/inbox-status
+    Description: Easy widget and functions to show email counts in your theme (Like unread email)
+    Version: 0.1
+    Author: Ben Lobaugh
+    Author URI: http://ben.lobaugh.net
+*/
+
+require_once( 'class.BlobImap.php' );
+require_once( 'class.EmailWidget.php' );

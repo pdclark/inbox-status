393d084ce9ca7a0e06b28ab8fc19b5d456a6956d
Paul Clark <pdclark@brainstormmedia.com>
2013-11-07 15:39:21 -0900

    Simplify plugin loader

--- plugin.php
+++ plugin.php
@@ -6,6 +6,8 @@ Description: Easy widget and functions to show email counts in your theme (like 
 Version: 1.0
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 /**
@@ -45,32 +47,11 @@ define( 'IS_PLUGIN_NAME', __( 'Inbox Status', IS_PLUGIN_SLUG ) );
 set_include_path( IS_PLUGIN_DIR . '/classes/PEAR' . PATH_SEPARATOR . get_include_path() );
 
 /**
- * Load plugin dependencies and instantiate the plugin.
- * Checks PHP version. Deactivates plugin and links to instructions if running PHP 4.
+ * Verify that we're running WordPress 3.2 (which enforces PHP 5.2.4).
  */
-function storm_inbox_status_init() {
-	
-	// PHP Version Check
-	$php_is_outdated = version_compare( PHP_VERSION, '5.2', '<' );
+if ( version_compare( $GLOBALS['wp_version'], '3.2', '>=' ) ) :
 
-	// Only exit and warn if on admin page
-	$okay_to_exit = is_admin() && ( !defined('DOING_AJAX') || !DOING_AJAX );
-	
-	if ( $php_is_outdated ) {
-		if ( $okay_to_exit ) {
-			require_once ABSPATH . '/wp-admin/includes/plugin.php';
-			deactivate_plugins( __FILE__ );
-			wp_die( sprintf( __(
-				'%s requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself. For information on upgrading, %ssee this article%s.', IS_PLUGIN_SLUG ),
-				IS_PLUGIN_NAME,
-				'<a href="http://codex.wordpress.org/Switching_to_PHP5" target="_blank">',
-				'</a>'
-			) );
-		} else {
-			return;
-		}
-	}
-
+	// Load plugin classes and instantiate the plugin.
 	if ( !class_exists( 'Net_IMAP' ) ) {
 		require_once IS_PLUGIN_DIR . '/classes/PEAR/Net/IMAP.php';
 	}
@@ -79,8 +60,6 @@ function storm_inbox_status_init() {
 	require_once IS_PLUGIN_DIR . '/classes/class-nav-menus.php';
 	require_once IS_PLUGIN_DIR . '/classes/class-inbox-status.php';
 
-	IS_Inbox_Status::get_instance();
+	add_action( 'plugins_loaded', 'IS_Inbox_Status::get_instance' );
 
-}
-
-add_action( 'plugins_loaded', 'storm_inbox_status_init' );
+endif;
